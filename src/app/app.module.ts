5484589950d861ad0b7c4a231f5680e821be8c3d
kelly-david <davidkelly.tlf@gmail.com>
2018-05-20 07:29:36 +0100

    Auth Guard Core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyCLJyD6ZJvDEXdN_OGJwY-I0S7cCbU8cTs',
+    authDomain: 'turra-lodge-farm.firebaseapp.com',
+    databaseURL: 'https://turra-lodge-farm.firebaseio.com',
+    projectId: 'turra-lodge-farm',
+    storageBucket: 'turra-lodge-farm.appspot.com',
+    messagingSenderId: '894927224700'
+  }
 };
